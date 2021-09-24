e938d1c1832a44679b7007d9d2807c9c849fb96f
David Velasco <davidvr1992@gmail.com>
2021-09-24 20:35:44 +0200

    https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,9 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Contracts\Routing\UrlGenerator
+
 Route::get('/', 'ConversationController@index');
 
 Route::post('conversation/message/{message}', 'ConversationController@sendMessage');
 Route::get('conversation/history/', 'ConversationController@getHistory');
 
 Route::post('conversation/graphql/{petition}', 'ConversationController@getList');
+
+URL::forceScheme('https');
